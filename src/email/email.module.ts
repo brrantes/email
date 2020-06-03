7f599a1324f757d224df828d8dddbdad7f9dd48a
Alexander Barrantes <alexander.barrantes@gamanza.com>
2020-06-03 09:36:35 -0600

    version

--- src/email/email.module.ts
+++ src/email/email.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
-import { EmailService } from "../email/Email.service"
-import { EmailController } from "../email/email.controller"
+import { EmailService } from "./email.service"
+import { EmailController } from "./email.controller"
 import { MongooseModule } from "@nestjs/mongoose"
-import { EmailSchema } from "../email/schemas/email.schema"
+import { EmailSchema } from "./schemas/email.schema"
 
 @Module({
     imports: [MongooseModule.forFeature([
